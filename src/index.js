cb2c12645886e9b1e134e3cbe225225a42ba2b10
Iain Reid <iainreid382@gmail.com>
2017-09-11 20:59:29 +0100

    [chore] Drop version and add required code
    Added the required source for a working version and dropped the version
    number to start at an alpha release.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const apn = require('apn')
+
+exports.register = function (server, options, next) {
+  let connection;
+
+  if (options) {
+    connection = new apn.Provider(options)
+  }
+
+  server.decorate('server', 'apn', {
+    connection,
+    Notification: apn.Notification,
+    Provider: apn.Provider
+  })
+
+  next()
+}
+
+exports.register.attributes = {
+  name: 'hapi-apn'
+}
